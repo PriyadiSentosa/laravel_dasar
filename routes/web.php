f939d5a2754a3639e63a804bf6d40da8999c4fa1
PriyadiSentosa <priyadisentosa825@gmail.com>
2021-10-04 00:31:46 -0700

    parameter

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hallo pak';
 });
+
+Route::get('laravel', function () {
+    return 'hallo laravel';
+});
+
+Route::get('tambah', function () {
+    $total = 10 + 10;
+    return $total;
+});
+Route::get('kurang', function () {
+    $total = 10 - 10;
+    return $total;
+});
+Route::get('kali', function () {
+    $total = 10 * 10;
+    return $total;
+});
+Route::get('bagi', function () {
+    $total = 10 / 10;
+    return $total;
+});
+
+// paarameter wajib
+Route::get('/nama-framework/{nama}/{tahun}', function ($nama,$tahun) {
+    return 'Nama Framework yang dimasukan adalah : ' .$nama. '<br>Tahun <b>Rilis</b> : '.$tahun;
+});
+
+// optional parameter
+Route::get('/nama-saya-adalah/{nama?}', function ($nama = 'Wajib diisi') {
+    return 'Nama Saya adalah : ' . $nama;
+});
+
+Route::get('/hitung-luas-segitiga/{alas}/{tinggi}', function ($alas,$tinggi) {
+    $total = $alas * $tinggi;
+     return 'Alas Segitiga : ' .$alas. '<br>Tinggi Segitiga : '. $tinggi.'<br>Hasil Luas Segitiga Nya : '.$total;
+});
+
+Route::get('/luas-segitiga/{alas?}/{tinggi?}', function ($alas = 1 ,$tinggi = 1) {
+     return 'Luas Segitiga : '.($hasil = $alas * $tinggi) *0.5;
+});
+
